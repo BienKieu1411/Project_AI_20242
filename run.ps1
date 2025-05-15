fcd2717f380336be6aa36f161fa1d833120cb275
BienKieu <bien14112005@gmail.com>
2025-05-15 16:22:20 +0700

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,10 +1,28 @@
+# Cài dependencies nếu có requirements.txt
 if (Test-Path ".\requirements.txt") {
     py -m pip install -r requirements.txt
 }
 
-Start-Process py app.py
+Start-Process -NoNewWindow -FilePath py -ArgumentList "app.py"
+
 Start-Sleep 1
+function Test-LiveServer {
+    try {
+        $version = live-server --version
+        return $true
+    }
+    catch {
+        return $false
+    }
+}
 
-Start-Process "http://127.0.0.1:5500/index.html"
+if (Test-LiveServer) {
+    Start-Process -NoNewWindow -FilePath live-server -ArgumentList "--port=5500 --open=./index.html"
+}
+else {
+    Start-Process -NoNewWindow -FilePath py -ArgumentList "-m http.server 5500"
+    Start-Sleep 1
+    Start-Process "http://127.0.0.1:5500/index.html"
+}
 
-Read-Host "Server running. Press Enter to exit."
+Read-Host "Servers are running. Press Enter to exit."
